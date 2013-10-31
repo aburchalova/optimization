7dc6433eb415c594d6985d66aa500f9e7c048c66
Anastasia Burchalova <anas_bur@mail.ru>
2013-10-31 11:33:09 +0300

    Bug

---
+++ app/models/solvers/dual_simplex.rb
@@ -0,0 +1,25 @@
+module Solvers
+  class DualSimplex < Solvers::Base
+    def self.simple_init(a, b, c, plan, basis)
+      task, x = super
+      new(Tasks::DualSimplex.new(task, x))
+    end
+
+    def calculate_and_change_status
+      return @status.not_a_plan! if !task.basis_plan?
+      return @status.singular! if task.singular_basis_matrix?
+      return @status.optimal! if task.sufficient_for_optimal?
+      return @status.incompatible! if !task.has_step?
+      @status.step_completed!
+    end
+
+    def initialize(task_with_plan)
+      @initial_task = task_with_plan
+      @task = task_with_plan
+      @status = Statuses::Simplex[:initialized]
+      @new_task_composer_class = NewTaskComposers::DualSimplex
+      @logging = false
+      self
+    end
+  end
+end
